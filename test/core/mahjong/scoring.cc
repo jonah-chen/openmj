afbd2dcc905525e6773cf70101ae66ff4c6d4ef5
jonah-chen <jonah.chen@mail.utoronto.ca>
2022-06-10 22:58:58 -0600

    update to new syntax

--- test/core/mahjong/scoring.cc
+++ test/core/mahjong/scoring.cc
@@ -5,6 +5,6 @@
 int main()
 {
     mj::scoring::ScoringCombo sc;
-    mj::scoring::eval<mj::scoring::Yaku::riichi>(sc, mj::Hand(), mj::Win());
+    mj::scoring::eval<mj::scoring::Yaku::riichi>(sc, mj::Hand(), mj::Win(), mj::Tile());
     return g_FailureCount;
 }
