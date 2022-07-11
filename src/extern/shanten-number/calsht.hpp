b1c9806cdf315e860a245bded2dd5522dcd042cf
jonah-chen <jonah.chen@mail.utoronto.ca>
2022-07-10 18:27:53 -0600

    reformat calsht

--- src/extern/shanten-number/calsht.hpp
+++ src/extern/shanten-number/calsht.hpp
@@ -1,36 +1,40 @@
 #ifndef CALSHT_HPP
 #define CALSHT_HPP
 
-#include "core/mahjong/mahjong.hpp"
 #include "constant.hpp"
+#include "core/mahjong/mahjong.hpp"
 
 namespace mj {
-namespace ext{
+namespace ext {
 namespace tomohxx {
-class Calsht{
+class Calsht
+{
 private:
-  using LVec  = std::vector<int32_t>;
-  using RVec  = std::vector<int8_t>;
-  using Iter = std::vector<RVec>::iterator;
+    using LVec = std::vector<int32_t>;
+    using RVec = std::vector<int8_t>;
+    using Iter = std::vector<RVec>::iterator;
 
-  std::vector<RVec> mp1;
-  std::vector<RVec> mp2;
+    std::vector<RVec> mp1;
+    std::vector<RVec> mp2;
 
 #ifdef THREE_PLAYER
-  Vec index1(int n) const;
+    Vec index1(int n) const;
 #endif
-  void add1(LVec& lhs, const RVec& rhs, int m) const;
-  void add2(LVec& lhs, const RVec& rhs, int m) const;
-  Iter read_file(Iter first, Iter last, std::filesystem::path file) const;
-  void initialize(const std::string& dir);
+    void add1(LVec &lhs, const RVec &rhs, int m) const;
+    void add2(LVec &lhs, const RVec &rhs, int m) const;
+    Iter read_file(Iter first, Iter last, std::filesystem::path file) const;
+    void initialize(const std::string &dir);
 
 public:
-  Calsht(const std::string &dir) : mp1(1953125, RVec(10)), mp2(78125, RVec(10)) 
-  { initialize(dir); }
-  int calc_lh(const int* t, int m) const;
-  int calc_sp(const int* t) const;
-  int calc_to(const int* t) const;
-  std::tuple<int,int> operator()(const Hand4Hot& t, int m, int mode) const;
+    Calsht(const std::string &dir)
+        : mp1(1953125, RVec(10)), mp2(78125, RVec(10))
+    {
+        initialize(dir);
+    }
+    int calc_lh(const int *t, int m) const;
+    int calc_sp(const int *t) const;
+    int calc_to(const int *t) const;
+    std::tuple<int, int> operator()(const Hand4Hot &t, int m, int mode) const;
 };
 
 } // namespace tomohxx
--- src/extern/shanten-number/sample.cpp
+++ src/extern/shanten-number/sample.cpp
@@ -1,76 +1,90 @@
-#include <iostream>
-#include <iomanip>
-#include <array>
-#include <random>
-#include <algorithm>
-#include <chrono>
-#include <utility>
 #include "calsht.hpp"
+// #include <algorithm>
+// #include <array>
+// #include <chrono>
+// #include <iomanip>
+// #include <iostream>
+// #include <random>
+// #include <utility>
 
 using namespace mj::ext::tomohxx;
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-  if(argc != 3){
-    return 1;
-  }
+    if (argc != 3)
+    {
+        return 1;
+    }
 
 #ifdef THREE_PLAYER
-  const int T = 27;
+    const int T = 27;
 #else
-  const int T = 34;
+    const int T = 34;
 #endif
-  const int M = std::atoi(argv[1]);
-  const int N = std::atoi(argv[2]);
-  int n = 0;
-  double ev = 0;
-  mj::Hand4Hot hd(K, 0);
-  std::array<int,4*T> wall = {0};
-  std::array<int,8> sht = {0};
-  std::mt19937 rand(std::random_device{}());
-  Calsht calsht("../../../../assets/tables");
+    const int M = std::atoi(argv[1]);
+    const int N = std::atoi(argv[2]);
+    int n = 0;
+    double ev = 0;
+    mj::Hand4Hot hd(K, 0);
+    std::array<int, 4 *T> wall = {0};
+    std::array<int, 8> sht = {0};
+    std::mt19937 rand(std::random_device{}());
+    Calsht calsht("../../../../assets/tables");
 
-  auto itr = wall.begin();
+    auto itr = wall.begin();
 
-  for(int i=0; i<K; ++i){
+    for (int i = 0; i < K; ++i)
+    {
 #ifdef THREE_PLAYER
-    if(i>0 && i<8) continue;
+        if (i > 0 && i < 8)
+            continue;
 #endif
-    for(int j=0; j<4; ++j){
-      *itr++ = i;
+        for (int j = 0; j < 4; ++j)
+        {
+            *itr++ = i;
+        }
     }
-  }
 
-  auto start = std::chrono::system_clock::now();
+    auto start = std::chrono::system_clock::now();
 
-  for(int i=0; i<N; ++i){
-    std::fill(hd.begin(), hd.end(), 0);
+    for (int i = 0; i < N; ++i)
+    {
+        std::fill(hd.begin(), hd.end(), 0);
 
-    for(int j=0; j<M; ++j){
-      n = rand()%(4*T-j);
-      ++hd[wall[n]];
-      std::swap(wall[n],wall[4*T-1-j]);
+        for (int j = 0; j < M; ++j)
+        {
+            n = rand() % (4 * T - j);
+            ++hd[wall[n]];
+            std::swap(wall[n], wall[4 * T - 1 - j]);
+        }
+        auto [num, mode] = calsht(hd, M / 3, 7);
+        ++sht[num];
     }
-    auto [num, mode] = calsht(hd,M/3,7);
-    ++sht[num];
-  }
 
-  auto end = std::chrono::system_clock::now();
+    auto end = std::chrono::system_clock::now();
 
-  std::cout.setf(std::ios::left,std::ios::adjustfield);
+    std::cout.setf(std::ios::left, std::ios::adjustfield);
 
-  std::cout << "=========================RESULT=========================\n";
+    std::cout << "=========================RESULT=========================\n";
 
-  for(int i=0; i<8; i++){
-    std::cout << std::setw(4) << (i-1) << std::setw(12) << sht[i] << std::setw(12) << 100.0*sht[i]/N << '\n';
-    ev += (i-1)*sht[i];
-  }
-  ev /= N;
+    for (int i = 0; i < 8; i++)
+    {
+        std::cout << std::setw(4) << (i - 1) << std::setw(12) << sht[i]
+                  << std::setw(12) << 100.0 * sht[i] / N << '\n';
+        ev += (i - 1) * sht[i];
+    }
+    ev /= N;
 
-  std::cout << std::setw(24) << "Number of Tiles" << std::setw(16) << M << '\n';
-  std::cout << std::setw(24) << "Total" << std::setw(16) << N << '\n';
-  std::cout << std::setw(24) << "Time (msec.)" << std::setw(16) << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << '\n';
-  std::cout << std::setw(24) << "Expected Value" << std::setw(24) << ev << '\n';
+    std::cout << std::setw(24) << "Number of Tiles" << std::setw(16) << M
+              << '\n';
+    std::cout << std::setw(24) << "Total" << std::setw(16) << N << '\n';
+    std::cout << std::setw(24) << "Time (msec.)" << std::setw(16)
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end -
+                                                                       start)
+                     .count()
+              << '\n';
+    std::cout << std::setw(24) << "Expected Value" << std::setw(24) << ev
+              << '\n';
 
-  return 0;
+    return 0;
 }
